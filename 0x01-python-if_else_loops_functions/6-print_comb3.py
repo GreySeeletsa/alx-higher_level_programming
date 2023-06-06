3add57ad2b5f91facebef29bef378440d0f279aa
GreySeeletsa <seeletsagrey@gmail.com>
2023-06-06 13:00:50 -0700

    6-print_comb3.py file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# this program prints all possible different combinations of 2 digits
+for numbers1 in range(0, 10):
+    for numbers2 in range(numbers1 + 1, 10):
+        if numbers1 == 8 and numbers2 == 9:
+            print('{}{}'.format(numbers1, numbers2))
+        else:
+            print('{}{}'.format(numbers1, numbers2), end=',')
