2ccee1bcfd7cbaf11476c8c085a8ea0d5e285d44
GreySeeletsa <seeletsagrey@gmail.com>
2023-06-08 14:47:40 -0700

    handle 100-my_calculator.py output

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,10 +8,12 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        allowed_optr = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(allowed_optr.keys()):
+        allowed_ops = {"+": add, "-": sub, "*": mul, "/": div}
+        if sys.argv[2] not in list(allowed_ops.keys()):
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.ext(1)
+            sys.exit(1)
+
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, allowed_optr[sys.argv[2]](a, b)))
+        print("{} {} {} = {}".format(a, sys.argv[2], b, (
+            allowed_ops[sys.argv[2]](a, b))))
