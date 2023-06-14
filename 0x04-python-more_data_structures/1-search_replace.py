3a05b67516bb7f7afd5c591cb35f2a0479f6f688
GreySeeletsa <seeletsagrey@gmail.com>
2023-06-14 12:11:18 -0700

    returns a list with all values multiplied by a number

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n_list = list(map(lambda x: replace if x == search each x, my_list))
+    n_list = list(map(lambda x: replace if x == search else x, my_list))
     return (n_list)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    n_directionary = a_dictionary.copy()
-    k_list = list(n_direction.keys())
+    n_dictionary = a_dictionary.copy()
+    k_list = list(n_dictionary.keys())
     for x in k_list:
-        n_directionary[x] *= 2
-        return (n_directionary)
+        n_dirctionary[x] *= 2
+        return (n_dictionary)
