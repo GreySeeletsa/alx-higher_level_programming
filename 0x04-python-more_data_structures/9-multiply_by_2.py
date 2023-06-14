ecabfcc54e551a6265f89f3690811efab76e65e9
GreySeeletsa <seeletsagrey@gmail.com>
2023-06-14 12:51:32 -0700

    fix output error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     n_dict = a_dictionary.copy()
     k_list = list(n_dict.keys())
     for x in k_list:
-        n_dirct[x] *= 2
+        n_dict[x] *= 2
     return (n_dict)
