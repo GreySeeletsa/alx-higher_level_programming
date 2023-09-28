fe34dbca49b801c52a12a5952aec89e9988c76db
GreySeeletsa <seeletsagrey@gmail.com>
2023-09-28 10:11:14 -0700

    script that takes in URL as an arg, sends a GET request to it, and displays body of response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# that takes in URL as an arg, sends a GET request to it, and displays body of response
+# takes in URL as an arg, sends a GET request to it, and displays body of response
 curl -s "$1" -H "X-School-User-Id: 98"
