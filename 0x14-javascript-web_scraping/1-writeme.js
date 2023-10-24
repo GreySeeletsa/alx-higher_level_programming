72e239657e428e27ca9cce288c16ab597debb6af
GreySeeletsa <seeletsagrey@gmail.com>
2023-10-24 05:20:05 -0700

    handle semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 // The first arg is the file path
 const file = process.argv[2];
-const text = process.argv[3]
+const text = process.argv[3];
 fs.writeFile(file, text, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
